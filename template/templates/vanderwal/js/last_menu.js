eae816e6b95e386bb939bcc81675e670798a25ae
Jonathan Bennett <jbennett@ccistudios.com>
2011-03-07 08:28:36 -0500

    Updated mod_mainmenu to include a .last and .first class

--- template/templates/vanderwal/js/last_menu.js
+++
@@ -1,8 +0,0 @@
-window.addEvent('domready', function() {
-	$$('ul.menu').each(function(menu) {
-		var lis = menu.getElements('li');
-
-		if (lis.length > 0)
-			lis[lis.length-1].addClass('last');
-	});
-});
