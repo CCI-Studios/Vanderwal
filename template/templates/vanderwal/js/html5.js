fff29ab703b980bbf898f2bba4246e8028e99d3c
Jonathan Bennett <jbennett@ccistudios.com>
2011-03-07 14:29:24 -0500

    Added placeholder js

---
+++ template/templates/vanderwal/js/html5.js
@@ -0,0 +1,30 @@
+/* placeholder text */
+(function (window, document, undefined) {
+	window.addEvent('domready', function() {
+		var i = document.createElement('input');
+
+		if ('placeholder' in i) {
+			return;
+		}
+
+		$$('input[type="text"]').each(function(input) {
+			var placeholder;
+
+			if (input.getProperty('placeholder')) {
+				placeholder = input.getProperty('placeholder');
+
+				input.addEvents({
+					focus: function() {
+						if (input.value === placeholder)
+							input.value = "";
+
+					},
+					blur: function() {
+						if (input.value === "")
+							input.value = placeholder;
+					}
+				});
+			}
+		});
+	});
+})(this, this.document);
